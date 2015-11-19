1801a1f2bceb2ab75241fd09cf8ae72bb23bf92c
soonhyeon choi <choissgt@gmail.com>
2015-11-19 15:42:26 +0900

    Update StudentDao
    - method 순서 변경 (selectOne, selectList, insert, update, delete 알파벳 순)

--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/StudentDao.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/StudentDao.java
@@ -6,18 +6,18 @@ import org.redborn.csatlatte.domain.StudentVo;
 
 public interface StudentDao {
 	
-	public int selectOneCountPassword(int studentSequence, String password);
-	public int updatePassword(int studentSequence, String newPassword);
-	public int updateInformation(StudentVo studentVo);
-	public int selectOneMaxStudentSequence();
-	public int insert(StudentVo studentVo);
-	public String selectOneId(String nickname, String securityAnswer);
-	public int selectOneCountPassword(String id, String securityAnswer);
 	public int selectOne(String id, String password);
-	public int updateUseYnN(int studentSequence);
+	public int selectOneCountPassword(int studentSequence, String password);
+	public int selectOneCountIsPassword(String id, String securityAnswer);
+	public String selectOneId(String nickname, String securityAnswer);
+	public int selectOneMaxStudentSequence();
+	public List<StudentVo> selectList(String studentId, String nickname);
 	public List<StudentVo> selectListCountYmd(String ymd);
 	public List<StudentVo> selectListCountYm(String ym);
 	public List<StudentVo> selectListCountYear(String year);
-	public List<StudentVo> selectList(String studentId, String nickname);
+	public int insert(StudentVo studentVo);
+	public int updateInformation(StudentVo studentVo);
+	public int updatePassword(int studentSequence, String newPassword);
+	public int updateUseYnN(int studentSequence);
 	
 }
--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/StudentDaoMapper.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/StudentDaoMapper.java
@@ -11,12 +11,64 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class StudentDaoMapper extends SqlSessionDaoSupport implements StudentDao {
 
+	public int selectOne(String id, String password) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", id);
+		map.put("password", password);
+		
+		return getSqlSession().selectOne("student.selectOne", map);
+	}
+	
 	public int selectOneCountPassword(int studentSequence, String password) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("studentSequence", studentSequence);
 		map.put("password",	password);
 		
 		return getSqlSession().selectOne("student.selectOneCountPassword", map);
+	}
+	
+	public int selectOneCountIsPassword(String id, String securityAnswer) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", id);
+		map.put("securityAnswer", securityAnswer);
+		
+		return getSqlSession().selectOne("student.selectOneCountIsPassword", map);
+	}
+	
+	public String selectOneId(String nickname, String securityAnswer) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("nickname", nickname);
+		map.put("securityAnswer", securityAnswer);
+		
+		return getSqlSession().selectOne("student.selectOneId", map);
+	}
+	
+	public int selectOneMaxStudentSequence() {
+		return getSqlSession().selectOne("student.selectOneMaxStudentSequence");
+	}
+	
+	public List<StudentVo> selectList(String studentId, String nickname) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("studentId", studentId);
+		map.put("nickname", nickname);
+		
+		return getSqlSession().selectList("student.selectList", map);
+	}
+
+	public List<StudentVo> selectListCountYmd(String ymd) {
+		return getSqlSession().selectList("student.selectListCountYmd", ymd);
+	}
+
+	public List<StudentVo> selectListCountYm(String ym) {
+		return getSqlSession().selectList("student.selectListCountYm", ym);
+	}
+
+	public List<StudentVo> selectListCountYear(String year) {
+		return getSqlSession().selectList("student.selectListCountYear", year);
+	}
+	
+	public int insert(StudentVo studentVo) {
+		return getSqlSession().insert("student.insert", studentVo);
 	}
 
 	public int updatePassword(int studentSequence, String newPassword) {
@@ -31,60 +83,8 @@ public class StudentDaoMapper extends SqlSessionDaoSupport implements StudentDao
 		return getSqlSession().update("student.updateInformation", studentVo);
 	}
 
-	public int selectOneMaxStudentSequence() {
-		return getSqlSession().selectOne("student.selectOneMaxStudentSequence");
-	}
-
-	public int insert(StudentVo studentVo) {
-		return getSqlSession().insert("student.insert", studentVo);
-	}
-
-	public String selectOneId(String nickname, String securityAnswer) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("nickname", nickname);
-		map.put("securityAnswer", securityAnswer);
-		
-		return getSqlSession().selectOne("student.selectOneId", map);
-	}
-
-	public int selectOneCountPassword(String id, String securityAnswer) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("id", id);
-		map.put("securityAnswer", securityAnswer);
-		
-		return getSqlSession().selectOne("student.selectOneCountPassword", map);
-	}
-
-	public int selectOne(String id, String password) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("id", id);
-		map.put("password", password);
-		
-		return getSqlSession().selectOne("student.selectOne", map);
-	}
-
 	public int updateUseYnN(int studentSequence) {
 		return getSqlSession().update("student.updateUseYnN", studentSequence);
-	}
-
-	public List<StudentVo> selectListCountYmd(String ymd) {
-		return getSqlSession().selectList("student.selectListCountYmd", ymd);
-	}
-
-	public List<StudentVo> selectListCountYm(String ym) {
-		return getSqlSession().selectList("student.selectListCountYm", ym);
-	}
-
-	public List<StudentVo> selectListCountYear(String year) {
-		return getSqlSession().selectList("student.selectListCountYear", year);
-	}
-
-	public List<StudentVo> selectList(String studentId, String nickname) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("studentId", studentId);
-		map.put("nickname", nickname);
-		
-		return getSqlSession().selectList("student.selectList", map);
 	}
 
 }
