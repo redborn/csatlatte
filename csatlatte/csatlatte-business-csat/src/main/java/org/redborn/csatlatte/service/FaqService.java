89572e2c7c7ce8965ac0f0277db16b5042bd92f6
soonhyeon choi <choissgt@gmail.com>
2015-11-19 23:41:24 +0900

    Add FaqService

--- csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/service/FaqService.java
+++ csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/service/FaqService.java
@@ -3,9 +3,11 @@ package org.redborn.csatlatte.service;
 import java.util.List;
 
 import org.redborn.csatlatte.domain.FaqTypeVo;
+import org.redborn.csatlatte.domain.FaqVo;
 
 public interface FaqService {
 	
+	public List<FaqVo> list(int faqTypeSequence);
 	public List<FaqTypeVo> typeList();
 
 }
--- csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/service/FaqServiceImpl.java
+++ csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/service/FaqServiceImpl.java
@@ -3,6 +3,8 @@ package org.redborn.csatlatte.service;
 import java.util.List;
 
 import org.redborn.csatlatte.domain.FaqTypeVo;
+import org.redborn.csatlatte.domain.FaqVo;
+import org.redborn.csatlatte.persistence.FaqDao;
 import org.redborn.csatlatte.persistence.faq.TypeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,12 @@ public class FaqServiceImpl implements FaqService {
 
 	@Autowired
 	private TypeDao typeDao;
+	@Autowired
+	private FaqDao faqDao;
+	
+	public List<FaqVo> list(int faqTypeSequence) {
+		return faqDao.selectList(faqTypeSequence);
+	}
 	
 	public List<FaqTypeVo> typeList() {
 		return typeDao.selectList();
