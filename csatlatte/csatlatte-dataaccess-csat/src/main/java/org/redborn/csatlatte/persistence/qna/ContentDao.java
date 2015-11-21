30a45531ab9f9ca826ed7e34e5948c4ebed81d17
soonhyeon choi <choissgt@gmail.com>
2015-11-21 16:21:06 +0900

    Update ContentDao
    - 반환 자료형 List<QnaContentVo> 로 변경

--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/qna/ContentDao.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/qna/ContentDao.java
@@ -1,10 +1,12 @@
 package org.redborn.csatlatte.persistence.qna;
 
+import java.util.List;
+
 import org.redborn.csatlatte.domain.QnaContentVo;
 
 public interface ContentDao {
 	
-	public String selectList(int qnaSequence);
+	public List<QnaContentVo> selectList(int qnaSequence);
 	public int insert(QnaContentVo qnaContentVo);
 	
 }
--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/qna/ContentDaoMapper.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/qna/ContentDaoMapper.java
@@ -1,5 +1,7 @@
 package org.redborn.csatlatte.persistence.qna;
 
+import java.util.List;
+
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.redborn.csatlatte.domain.QnaContentVo;
 import org.springframework.stereotype.Repository;
@@ -7,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ContentDaoMapper extends SqlSessionDaoSupport implements ContentDao {
 
-	public String selectList(int qnaSequence) {
+	public List<QnaContentVo> selectList(int qnaSequence) {
 		return getSqlSession().selectOne("qna.content.selectList", qnaSequence);
 	}
 
