e100e33f570e4d11d4dd1caa9eb96b987be49e06
soonhyeon choi <choissgt@gmail.com>
2015-11-26 15:39:29 +0900

    Update QnaDao.java
    - selectOne의 반환형 QnaVo로 변경

--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/QnaDao.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/QnaDao.java
@@ -1,13 +1,15 @@
 package org.redborn.csatlatte.persistence;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.redborn.csatlatte.domain.QnaForManageVo;
 import org.redborn.csatlatte.domain.QnaForStudentVo;
+import org.redborn.csatlatte.domain.QnaVo;
 
 public interface QnaDao {
 
-	public String selectOne(int qnaSequence);
+	public QnaVo selectOne(int qnaSequence);
 	public int selectOneMaxQnaSequence();
 	public List<QnaForManageVo> selectListForManage(String search, int begin);
 	public List<QnaForStudentVo> selectListForStudent(int studentSequence, int begin);
