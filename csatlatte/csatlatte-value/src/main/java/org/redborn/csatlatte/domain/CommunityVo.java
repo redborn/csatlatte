a4c7af8fdc7f3bc8bd7d2a22dc4a9c1cb608a0bf
soonhyeon choi <choissgt@gmail.com>
2015-11-28 20:02:59 +0900

    Add CommunityService
    - branch switch를 위한 임시 커밋

---
+++ csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/CommunityVo.java
@@ -0,0 +1,13 @@
+package org.redborn.csatlatte.domain;
+
+public class CommunityVo {
+
+	private int communityTypeSequence;
+	private int communitySeuqnece;
+	private int studentSequence;
+	private int content;
+	private int writeDate;
+	
+	
+	
+}
