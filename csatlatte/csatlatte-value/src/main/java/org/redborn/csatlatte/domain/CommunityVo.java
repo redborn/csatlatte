98fb4d00d9266bb2c6e8ce7324d46caa3769497f
soonhyeon choi <choissgt@gmail.com>
2015-11-29 10:35:21 +0900

    Update CommunityVo

--- csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/CommunityVo.java
+++ csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/CommunityVo.java
@@ -2,12 +2,48 @@ package org.redborn.csatlatte.domain;
 
 public class CommunityVo {
 
-	private int communityTypeSequence;
-	private int communitySeuqnece;
-	private int studentSequence;
+	private int communitySequence;
+	private String nickname;
 	private int content;
 	private int writeDate;
 	
+	public int getCommunitySequence() {
+		return communitySequence;
+	}
 	
+	public void setCommunitySequence(int communitySequence) {
+		this.communitySequence = communitySequence;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public int getContent() {
+		return content;
+	}
+	
+	public void setContent(int content) {
+		this.content = content;
+	}
+	
+	public int getWriteDate() {
+		return writeDate;
+	}
+	
+	public void setWriteDate(int writeDate) {
+		this.writeDate = writeDate;
+	}
+
+	@Override
+	public String toString() {
+		return "CommunityVo [communitySequence=" + communitySequence
+				+ ", nickname=" + nickname + ", content=" + content
+				+ ", writeDate=" + writeDate + "]";
+	}
 	
 }
