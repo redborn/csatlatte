aaa2ba0485df3bf5170c4c22e14de78b7d0070f9
darkelfand <darkelfand@gmail.com>
2015-12-05 00:14:46 +0900

    Update HttpSessionValue
    - 주석 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/commons/servlet/http/HttpSessionValue.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/commons/servlet/http/HttpSessionValue.java
@@ -5,6 +5,12 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * HttpSession의 값을 처리 합니다.
+ * 
+ * @author 최순열
+ *
+ */
 @Service
 public class HttpSessionValue {
 	
@@ -16,38 +22,81 @@ public class HttpSessionValue {
 	@Autowired
 	private HttpSession session;
 	
+	/**
+	 * 사용자를 입력 합니다.
+	 * 
+	 * @param studentSequence 학생 일련번호
+	 * @param nickname 닉네임
+	 * @param ruleSequence 규칙 일련번호
+	 */
 	public void setUser(int studentSequence, String nickname, int ruleSequence) {
 		this.session.setAttribute(STUDENT_SEQUENCE, studentSequence);
 		this.session.setAttribute(NICKNAME, nickname);
 		this.session.setAttribute(RULE_SEQUENCE, ruleSequence);
 	}
 	
+	/**
+	 * 학생 일련번호 입니다.
+	 * 
+	 * @return 학생 일련번호
+	 */
 	public int getStudentSequence() {
 		return getStudentSequence(this.session);
 	}
 	
+	/**
+	 * 학생 일련번호 입니다.
+	 * 
+	 * @param session 세션
+	 * @return 학생 일련번호
+	 */
 	public static int getStudentSequence(HttpSession session) {
 		Integer studentSequence = (Integer) session.getAttribute(STUDENT_SEQUENCE);
 		return studentSequence != null ? studentSequence : 0;
 	}
 	
+	/**
+	 * 규칙 일련번호 입니다.
+	 * 
+	 * @return 규칙 일련번호
+	 */
 	public int getRuleSequence() {
 		return getRuleSequence(this.session);
 	}
 	
+	/**
+	 * 규칙 일련번호 입니다.
+	 * 
+	 * @param session 세션
+	 * @return 규칙 일련번호
+	 */
 	public static int getRuleSequence(HttpSession session) {
 		Integer ruleSequence = (Integer) session.getAttribute(RULE_SEQUENCE);
 		return ruleSequence != null ? ruleSequence : 0;
 	}
 	
+	/**
+	 * 닉네임 입니다.
+	 * 
+	 * @return 닉네임
+	 */
 	public String getNickname() {
 		return getNickname(this.session);
 	}
 	
+	/**
+	 * 닉네임 입니다.
+	 * 
+	 * @param session 세션
+	 * @return 닉네임
+	 */
 	public static String getNickname(HttpSession session) {
 		return (String) session.getAttribute(NICKNAME);
 	}
 	
+	/**
+	 * 세션을 초기화 합니다.
+	 */
 	public void invalidate() {
 		this.session.invalidate();
 	}
