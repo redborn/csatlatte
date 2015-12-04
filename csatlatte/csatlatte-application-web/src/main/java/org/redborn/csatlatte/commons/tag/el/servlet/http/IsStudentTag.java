3212b60da8e31e7bdae417f35e9e0d15d945779d
darkelfand <darkelfand@gmail.com>
2015-12-05 00:12:28 +0900

    Add HttpSession tag

---
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/commons/tag/el/servlet/http/IsStudentTag.java
@@ -0,0 +1,22 @@
+package org.redborn.csatlatte.commons.tag.el.servlet.http;
+
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.jstl.core.ConditionalTagSupport;
+
+import org.redborn.csatlatte.commons.servlet.http.HttpSessionValue;
+
+/**
+ * 학생 여부를 판단하는 Taglib 입니다.
+ * 
+ * @author 최순열
+ *
+ */
+@SuppressWarnings("serial")
+public class IsStudentTag extends ConditionalTagSupport {
+
+	@Override
+	protected boolean condition() throws JspTagException {
+		return HttpSessionValue.getRuleSequence(pageContext.getSession()) == HttpSessionValue.STUDENT;
+	}
+
+}
