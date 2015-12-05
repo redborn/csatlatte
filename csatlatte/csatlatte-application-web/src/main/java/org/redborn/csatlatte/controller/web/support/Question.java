6e79952d3c5736ea2a6dd00ca37ddc6c11cf66b8
soonhyeon choi <choissgt@gmail.com>
2015-12-05 10:48:59 +0900

    Update Question
    - post 메소드에 파라미터 추가
    - qnaService 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/support/Question.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/support/Question.java
@@ -1,9 +1,13 @@
 package org.redborn.csatlatte.controller.web.support;
 
 import org.redborn.csatlatte.commons.tiles.TilesName;
+import org.redborn.csatlatte.domain.QnaVo;
+import org.redborn.csatlatte.service.QnaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +22,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Question {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-
+	@Autowired
+	private QnaService qnaService;
 	/**
 	 * 문의 제목, 내용을 입력하는 페이지입니다.
 	 */
 	@RequestMapping(method=RequestMethod.GET)
-	public String get() {
+	public String get(Model model) {
 		logger.info("support question view");
 		return TilesName.SUPPORT_QUESTION_WRITE;
 	}
@@ -36,9 +41,12 @@ public class Question {
 	 * SUPPORT_QUESTION_SUCCESS : 문의하기 완료
 	 */
 	@RequestMapping(method=RequestMethod.POST)
-	public String post(@RequestParam(value="success",required=false,defaultValue="0") int success) {
+	public String post(@RequestParam(value="title",required=true) String title, @RequestParam(value="content", required=true) String content) {
+		QnaVo qnaVo = new QnaVo();
+		qnaVo.setTitle(title);
+		qnaVo.setContent(content);
 		logger.info("support question write");
-		String result = TilesName.SUPPORT_QUESTION_SUCCESS;
-		return result;
+		// qnaService.write(qnaVo, null);
+		return TilesName.SUPPORT_QUESTION_SUCCESS;
 	}
 }
