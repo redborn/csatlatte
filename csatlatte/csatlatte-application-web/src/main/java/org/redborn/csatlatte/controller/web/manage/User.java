9fbd6a625f6a1d1c3edbfe3f05820970f9566637
soonhyeon choi <choissgt@gmail.com>
2015-12-05 21:31:36 +0900

    Update User.java
    - UserService 연결 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/manage/User.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/manage/User.java
@@ -1,11 +1,15 @@
 package org.redborn.csatlatte.controller.web.manage;
 
 import org.redborn.csatlatte.commons.tiles.TilesName;
+import org.redborn.csatlatte.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 사용자 정보를 조회하고 관리자 권한, 계정에 대한 차단 등을 설정하는 controller 입니다.
@@ -17,13 +21,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class User {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private StudentService studentService;
 
 	/**
 	 * 사용자 정보를 관리하는 페이지입니다.
 	 */
 	@RequestMapping(method=RequestMethod.GET)
-	public String get() {
+	public String get(Model model, @RequestParam(value="search",required=false,defaultValue="") String search, @RequestParam(value="pageNumber",required=false,defaultValue="0") int pageNumber) {
 		logger.info("manage user view");
+		model.addAttribute("userList", studentService.userList(search, pageNumber));
 		return TilesName.MANAGE_USER;
 	}
 }
