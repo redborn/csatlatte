4ccee2f98a94e47b2d13fab87ea128069e711f94
darkelfand <darkelfand@gmail.com>
2015-12-09 08:49:32 +0900

    Update comment mapper
    - selectList writeDate --> writeYmdhms 변경

--- csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/CommentVo.java
+++ csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/CommentVo.java
@@ -1,15 +1,15 @@
 package org.redborn.csatlatte.domain;
 
 public class CommentVo {
-	
+
 	private int communityTypeSequence;
 	private int communitySequence;
 	private int commentSequence;
 	private int studentSequence;
 	private String nickname;
 	private String content;
-	private String writeDate;
-	
+	private String writeYmdhms;
+
 	public int getCommunityTypeSequence() {
 		return communityTypeSequence;
 	}
@@ -21,49 +21,49 @@ public class CommentVo {
 	public int getCommunitySequence() {
 		return communitySequence;
 	}
-	
+
 	public void setCommunitySequence(int communitySequence) {
 		this.communitySequence = communitySequence;
 	}
-	
+
 	public int getCommentSequence() {
 		return commentSequence;
 	}
-	
+
 	public void setCommentSequence(int commentSequence) {
 		this.commentSequence = commentSequence;
 	}
-	
+
 	public int getStudentSequence() {
 		return studentSequence;
 	}
-	
+
 	public void setStudentSequence(int studentSequence) {
 		this.studentSequence = studentSequence;
 	}
-	
+
 	public String getNickname() {
 		return nickname;
 	}
-	
+
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
-	
+
 	public String getContent() {
 		return content;
 	}
-	
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
-	public String getWriteDate() {
-		return writeDate;
+
+	public String getWriteYmdhms() {
+		return writeYmdhms;
 	}
-	
-	public void setWriteDate(String writeDate) {
-		this.writeDate = writeDate;
+
+	public void setWriteYmdhms(String writeYmdhms) {
+		this.writeYmdhms = writeYmdhms;
 	}
 
 	@Override
@@ -72,7 +72,7 @@ public class CommentVo {
 				+ ", communitySequence=" + communitySequence
 				+ ", commentSequence=" + commentSequence + ", studentSequence="
 				+ studentSequence + ", nickname=" + nickname + ", content="
-				+ content + ", writeDate=" + writeDate + "]";
+				+ content + ", writeYmdhms=" + writeYmdhms + "]";
 	}
 
 }
