84ea1de8bb16585295e5cac05142d6430ae3d339
soonhyeon choi <choissgt@gmail.com>
2015-12-18 16:53:58 +0900

    Update User.java

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/manage/User.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/manage/User.java
@@ -1,6 +1,9 @@
 package org.redborn.csatlatte.controller.web.manage;
 
+import java.util.List;
+
 import org.redborn.csatlatte.commons.tiles.TilesName;
+import org.redborn.csatlatte.domain.StudentVo;
 import org.redborn.csatlatte.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/manage/user")
 public class User {
-	
+
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private StudentService studentService;
@@ -28,8 +31,42 @@ public class User {
 	 * 사용자 정보를 관리하는 페이지입니다.
 	 */
 	@RequestMapping(method=RequestMethod.GET)
-	public String get(Model model, @RequestParam(value="search",required=false,defaultValue="") String search, @RequestParam(value="pageNumber",required=false,defaultValue="0") int pageNumber) {
+	public String get(Model model, @RequestParam(value="search",required=false,defaultValue="") String search, @RequestParam(value="pageNumber",required=false,defaultValue="0") int pageNumber,
+			@RequestParam(value="beginPageNumber",required=false,defaultValue="1") int beginPageNumber) {
 		logger.info("manage user view");
+		
+		int amountStudent = studentService.amountStudent();
+		int endPageNumber;
+		int maxPageNumber = amountStudent / 10;
+		
+		if (amountStudent % 10 != 0) {
+			maxPageNumber++;
+		}
+		
+		int selectedPageNumber = beginPageNumber;
+		beginPageNumber = (beginPageNumber - 1) / 10 * 10 + 1;
+		endPageNumber = beginPageNumber + 9;
+		
+		if (endPageNumber > maxPageNumber) {
+			endPageNumber = maxPageNumber;
+		}
+		
+		if (pageNumber < 0) {
+			pageNumber = 0;
+		}
+		
+		if (beginPageNumber <= 0) {
+			beginPageNumber = 1;
+		}
+		
+		if (beginPageNumber > maxPageNumber) {
+			beginPageNumber = maxPageNumber;
+		}
+
+		model.addAttribute("selectedPageNumber", selectedPageNumber);
+		model.addAttribute("beginPageNumber", beginPageNumber);
+		model.addAttribute("endPageNumber", endPageNumber);
+		model.addAttribute("maxPageNumber", maxPageNumber);
 		model.addAttribute("userList", studentService.userList(search, pageNumber));
 		return TilesName.MANAGE_USER;
 	}
