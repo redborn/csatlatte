2f560a31af42e3c2162669415432afc902ff92c6
soonhyeon choi <choissgt@gmail.com>
2015-12-23 16:54:20 +0900

    Update Community
    - 블라인드 기능 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/manage/Community.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/manage/Community.java
@@ -21,10 +21,18 @@ public class Community {
 	@RequestMapping(method=RequestMethod.GET)
 	public void get(Model model, @RequestParam(value="communityTypeSequence",required=false,defaultValue="1") int communityTypeSequence,
 			@RequestParam(value="search",required=false,defaultValue="") String search, @RequestParam(value="pageNumber",required=false,defaultValue="1") int pageNumber) {
-		logger.info("data manage community");
+		logger.info("data manage community view");
 		
 		int beginPageNumber = (pageNumber * 10) - 10;
 		
 		model.addAttribute("list", communityService.list(communityTypeSequence, search, beginPageNumber));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public void post(@RequestParam(value="communityTypeSequence",required=true,defaultValue="1") int communityTypeSequence, 
+			@RequestParam(value="communitySequence",required=true) int communitySequence) {
+		logger.info("data manage community insert blind");
+		
+		communityService.blind(communityTypeSequence, communitySequence, "관리자에 의한 블라인드");
 	}
 }
