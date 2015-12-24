f0ec26cd2450b2e653f45e5c090b7130f17ea402
darkelfand <darkelfand@gmail.com>
2015-12-24 18:42:53 +0900

    Update controller data community
    -  커뮤니티 게시글(특정 회원) 리스트 출력 영역 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/Community.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/Community.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,6 +28,12 @@ public class Community {
 	public void get(Model model) {
 		logger.info("data community list");
 		model.addAttribute("list", communityService.list(CommunityService.COMMUNITY));
+	}
+	
+	@RequestMapping(value="{studentSequence}",method=RequestMethod.GET)
+	public void detail(Model model, @PathVariable int studentSequence) {
+		logger.info("data community detail");
+		communityService.list(CommunityService.COMMUNITY, studentSequence);
 	}
 	
 }
