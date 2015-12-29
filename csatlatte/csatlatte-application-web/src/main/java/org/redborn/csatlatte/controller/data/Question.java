e85d116ffc2960ba23e864fa4189421548556949
soonhyeon choi <choissgt@gmail.com>
2015-12-29 21:41:25 +0900

    Add data Question.java

---
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/Question.java
@@ -0,0 +1,29 @@
+package org.redborn.csatlatte.controller.data;
+
+import org.redborn.csatlatte.service.QnaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/data/question")
+public class Question {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private QnaService qnaService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public void get(Model model, @RequestParam(value="qnaSequence",required=true) int qnaSequence) {
+		logger.info("data question view");
+		
+		model.addAttribute("detail", qnaService.detail(qnaSequence));
+	}
+	
+
+}
