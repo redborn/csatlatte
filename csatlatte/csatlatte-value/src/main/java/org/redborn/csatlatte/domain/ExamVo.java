3d35313757245db6f9ed4e859c561eb20911bbb3
soonhyeon choi <choissgt@gmail.com>
2016-01-12 14:34:32 +0900

    Update naming
    - isttt > institution

--- csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/ExamVo.java
+++ csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/ExamVo.java
@@ -22,7 +22,7 @@ public class ExamVo {
 	public int getExamSequence() {
 		return examSequence;
 	}
-
+	
 	public void setExamSequence(int examSequence) {
 		this.examSequence = examSequence;
 	}
@@ -30,25 +30,17 @@ public class ExamVo {
 	public int getInstitutionSequence() {
 		return institutionSequence;
 	}
-
+	
 	public void setInstitutionSequence(int institutionSequence) {
 		this.institutionSequence = institutionSequence;
 	}
-
+	
 	public int getYsSequence() {
 		return ysSequence;
 	}
 	
 	public void setYsSequence(int ysSequence) {
 		this.ysSequence = ysSequence;
-	}
-
-	public String getExamName() {
-		return examName;
-	}
-
-	public void setExamName(String examName) {
-		this.examName = examName;
 	}
 	
 	public String getYmd() {
@@ -58,23 +50,31 @@ public class ExamVo {
 	public void setYmd(String ymd) {
 		this.ymd = ymd;
 	}
-
+	
 	public String getYear() {
 		return year;
 	}
-
+	
 	public void setYear(String year) {
 		this.year = year;
 	}
-
+	
+	public String getExamName() {
+		return examName;
+	}
+	
+	public void setExamName(String examName) {
+		this.examName = examName;
+	}
+	
 	public String getInstitutionName() {
 		return institutionName;
 	}
-
+	
 	public void setInstitutionName(String institutionName) {
 		this.institutionName = institutionName;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "ExamVo [csatSequence=" + csatSequence + ", examSequence="
@@ -83,5 +83,5 @@ public class ExamVo {
 				+ year + ", examName=" + examName + ", institutionName="
 				+ institutionName + "]";
 	}
-
+	
 }
