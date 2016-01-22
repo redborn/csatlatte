535cc9964576f0183128771c8bc915eb90d4136d
darkelfand <darkelfand@gmail.com>
2016-01-22 18:45:54 +0900

    Update Commons Amazonaws Service S3 FileDirectory
    - package 변경, 파일 경로 Amazon AWS S3로 변경

---
+++ csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/commons/amazonaws/services/s3/FileDirectory.java
@@ -0,0 +1,24 @@
+package org.redborn.csatlatte.commons.amazonaws.services.s3;
+
+/**
+ * Amazon S3 파일 디렉터리 입니다.
+ * 
+ * @author 최순열
+ *
+ */
+public class FileDirectory {
+	
+	/**
+	 * 임시 파일 디렉터리 입니다.
+	 */
+	public static final String TEMP = "temp";
+	/**
+	 * QnA 파일 디렉터리 입니다.
+	 */
+	public static final String QNA = "qna";
+	/**
+	 * 학생 Profile 파일 디렉터리 입니다.
+	 */
+	public static final String STUDENT_PROFILE = "student";
+
+}
