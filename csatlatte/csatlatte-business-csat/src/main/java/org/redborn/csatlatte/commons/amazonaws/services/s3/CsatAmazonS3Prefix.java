085e129478a896a159e04209fbd8da3ea005fb9e
darkelfand <darkelfand@gmail.com>
2016-01-24 15:34:31 +0900

    Update Commons Amazonaws SErvices S3 CastAmazonS3Prefix
    - Class Name 변경 FileDirectory --> CastAmazonS3Prefix

--- csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/commons/amazonaws/services/s3/CsatAmazonS3Prefix.java
+++ csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/commons/amazonaws/services/s3/CsatAmazonS3Prefix.java
@@ -1,23 +1,23 @@
 package org.redborn.csatlatte.commons.amazonaws.services.s3;
 
 /**
- * Amazon S3 파일 디렉터리 입니다.
+ * Amazon S3 Prefix 입니다.
  * 
  * @author 최순열
  *
  */
-public class FileDirectory {
+public class CsatAmazonS3Prefix {
 	
 	/**
-	 * 임시 파일 디렉터리 입니다.
+	 * 임시 Prefix 입니다.
 	 */
 	public static final String TEMP = "temp";
 	/**
-	 * QnA 파일 디렉터리 입니다.
+	 * QnA Prefix 입니다.
 	 */
 	public static final String QNA = "qna";
 	/**
-	 * 학생 Profile 파일 디렉터리 입니다.
+	 * 학생 Profile Prefix 입니다.
 	 */
 	public static final String STUDENT_PROFILE = "student";
 
