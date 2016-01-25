3a95f6be638f992e230543dade8cb14b8ff52427
darkelfand <darkelfand@gmail.com>
2016-01-25 13:03:13 +0900

    Update Data Access Layer Csat
    - selectOne(int csatSequence) 추가

--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/CsatDao.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/CsatDao.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.redborn.csatlatte.domain.CsatVo;
 
 public interface CsatDao {
-	
+
+	public CsatVo selectOne(int csatSequence);
 	public List<CsatVo> selectListYear();
+	
 }
