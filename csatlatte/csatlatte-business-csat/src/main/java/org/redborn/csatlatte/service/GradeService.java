b2e2e819e64eaef62444e5a816c1b5c11d3fae9e
darkelfand <darkelfand@gmail.com>
2016-01-29 20:48:46 +0900

    feat(business) : update grade
    dataccess layer exam student score selectList return 변경에 따른 list method
    변경
    GradeVo --> GradeListVo 변경

--- csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/service/GradeService.java
+++ csatlatte/csatlatte-business-csat/src/main/java/org/redborn/csatlatte/service/GradeService.java
@@ -2,6 +2,7 @@ package org.redborn.csatlatte.service;
 
 import java.util.List;
 
+import org.redborn.csatlatte.domain.GradeListVo;
 import org.redborn.csatlatte.domain.GradeRatingVo;
 import org.redborn.csatlatte.domain.GradeStandardscoreVo;
 import org.redborn.csatlatte.domain.GradeVo;
@@ -11,7 +12,7 @@ public interface GradeService {
 	public boolean register(GradeVo gradeVo);
 	public boolean modify(GradeVo gradeVo);
 	public boolean delete(int studentSequence, int csatSequence, int examSequence, int sectionSequence, int subjectSequence);
-	public List<GradeVo> list(int csatSequence, int examSequence, int studentSequence);
+	public List<GradeListVo> list(int csatSequence, int examSequence, int studentSequence);
 	public List<GradeRatingVo> ratingcutHistory(int csatSequence, int examSequence, int studentSequence);
 	public List<GradeStandardscoreVo> standardscoreHistory(int csatSequence, int examSequence, int studentSequence);
 	
