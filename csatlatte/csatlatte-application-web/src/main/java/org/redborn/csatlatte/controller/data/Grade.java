e8548cf4b86a8aa5df7fe4b48945ef8043f4dbbc
darkelfand <darkelfand@gmail.com>
2016-01-30 23:30:11 +0900

    feat(controller) : update data grade delete
    @RequestParam으로 처리한 sectionSequence, subjectSequence를 @PathVariable로 변경

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/Grade.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/Grade.java
@@ -55,8 +55,8 @@ public class Grade {
 		model.addAttribute("result", gradeService.modify(gradeVo));
 	}
 
-	@RequestMapping(value="{examSequence}",method=RequestMethod.DELETE)
-	public void delete(Model model, @PathVariable(value="examSequence") int examSequence, @RequestParam(value="sectionSequence",required=true) int sectionSequence, @RequestParam(value="subjectSequence",required=true) int subjectSequence) {
+	@RequestMapping(value="{examSequence}/{sectionSequence}/{subjectSequence}",method=RequestMethod.DELETE)
+	public void delete(Model model, @PathVariable(value="examSequence") int examSequence, @PathVariable(value="sectionSequence") int sectionSequence, @PathVariable(value="subjectSequence") int subjectSequence) {
 		logger.info("data grade delete");
 		model.addAttribute("result", gradeService.delete(httpSessionValue.getStudentSequence(), httpSessionValue.getCsatSequence(), examSequence, sectionSequence, subjectSequence));
 	}
