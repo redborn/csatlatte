aa7601d448f4ce6f2732ccb04ab9a34e0b0cac1b
soonhyeon choi <choissgt@gmail.com>
2016-02-11 16:32:45 +0900

    feat(controller) : update account default.class
    - 사용자 데이터 호출 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/account/Default.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/account/Default.java
@@ -1,9 +1,14 @@
 package org.redborn.csatlatte.controller.account;
 
+import org.redborn.csatlatte.commons.servlet.http.HttpSessionValue;
 import org.redborn.csatlatte.commons.tiles.TilesName;
+import org.redborn.csatlatte.service.ExamService;
+import org.redborn.csatlatte.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,10 +17,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class Default {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired 
+	private StudentService studentService;
+	@Autowired
+	private ExamService examService;
+	@Autowired
+	private HttpSessionValue httpSessionValue;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String get() {
+	public String get(Model model) {
 		logger.info("account default view");
+		int studentSequence = httpSessionValue.getStudentSequence();
+		int csatSequence = httpSessionValue.getCsatSequence();
+		model.addAttribute("csat", examService.csat(csatSequence));
+		model.addAttribute("securityQuestion", studentService.securityQuestion(studentSequence));
 		return TilesName.PROFILE;
 	}
 
