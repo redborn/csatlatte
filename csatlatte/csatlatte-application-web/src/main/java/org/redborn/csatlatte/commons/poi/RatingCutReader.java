af82cb73728229d4b2f191072d1e2c73e204c5b8
darkelfand <darkelfand@gmail.com>
2016-02-11 19:08:04 +0900

    feat(commons) : update rating cut reader
    엑셀 양식 변경에 따른 기능 변경

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/commons/poi/RatingCutReader.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/commons/poi/RatingCutReader.java
@@ -22,9 +22,98 @@ import org.redborn.csatlatte.domain.SubjectVo;
 public class RatingCutReader {
 	
 	private XSSFWorkbook workbook;
+	private int csatSequence;
+	private int examSequence;
+	private List<SectionVo> sectionVos;
+	private List<SubjectVo> subjectVos;
+	private List<RatingCutVo> ratingCutVos;
+	private List<AverageVo> averageVos;
 	
-	public RatingCutReader(InputStream inputStream) throws IOException {
+	public RatingCutReader(InputStream inputStream, int csatSequence, int examSequence) throws IOException {
 		workbook = new XSSFWorkbook(inputStream);
+		this.csatSequence = csatSequence;
+		this.examSequence = examSequence;
+		init();
+	}
+	
+	public void init() {
+		parseSectionList();
+		parseRatingCutList();
+		parseSubjectListAndAverageList();
+	}
+	
+	private void parseSectionList() {
+		XSSFSheet sectionSheet = workbook.getSheet("section");
+		if (sectionSheet != null) {
+			sectionVos = new ArrayList<SectionVo>();
+			SectionVo sectionVo;
+			XSSFRow csatExamSectionRow;
+			int physicalNumberOfRows = sectionSheet.getPhysicalNumberOfRows();
+			for (int rowIndex = 1; rowIndex < physicalNumberOfRows; rowIndex++) {
+				csatExamSectionRow = sectionSheet.getRow(rowIndex);
+				sectionVo = new SectionVo();
+				sectionVo.setCsatSequence(csatSequence);
+				sectionVo.setExamSequence(examSequence);
+				sectionVo.setSectionSequence((int) csatExamSectionRow.getCell(0).getNumericCellValue());
+				sectionVo.setSectionName(csatExamSectionRow.getCell(1).getStringCellValue());
+				sectionVo.setSelectCount((int) csatExamSectionRow.getCell(2).getNumericCellValue());
+				sectionVos.add(sectionVo);
+			}
+		}
+	}
+	
+	private void parseSubjectListAndAverageList() {
+		XSSFSheet subjectAndAverageSheet = workbook.getSheet("subject&average");
+		if (subjectAndAverageSheet != null) {
+			subjectVos = new ArrayList<SubjectVo>();
+			averageVos = new ArrayList<AverageVo>();
+			SubjectVo subjectVo;
+			AverageVo averageVo;
+			XSSFRow subjectAndRatingCutRow;
+			int physicalNumberOfRows = subjectAndAverageSheet.getPhysicalNumberOfRows();
+			for (int rowIndex = 1; rowIndex < physicalNumberOfRows; rowIndex++) {
+				subjectAndRatingCutRow = subjectAndAverageSheet.getRow(rowIndex);
+				subjectVo = new SubjectVo();
+				subjectVo.setCsatSequence(csatSequence);
+				subjectVo.setExamSequence(examSequence);
+				subjectVo.setSectionSequence((int) subjectAndRatingCutRow.getCell(0).getNumericCellValue());
+				subjectVo.setSubjectSequence((int) subjectAndRatingCutRow.getCell(1).getNumericCellValue());
+				subjectVo.setSubjectName(subjectAndRatingCutRow.getCell(2).getStringCellValue());
+				subjectVo.setMaxScore((int) subjectAndRatingCutRow.getCell(3).getNumericCellValue());
+				subjectVos.add(subjectVo);
+
+				averageVo = new AverageVo();
+				averageVo.setCsatSequence(csatSequence);
+				averageVo.setExamSequence(examSequence);
+				averageVo.setSectionSequence((int) subjectAndRatingCutRow.getCell(0).getNumericCellValue());
+				averageVo.setSubjectSequence((int) subjectAndRatingCutRow.getCell(1).getNumericCellValue());
+				averageVo.setAverage((float) subjectAndRatingCutRow.getCell(4).getNumericCellValue());
+				averageVo.setStandardDeviation((float) subjectAndRatingCutRow.getCell(5).getNumericCellValue());
+				averageVos.add(averageVo);
+			}
+		}
+	}
+	
+	private void parseRatingCutList() {
+		XSSFSheet ratingCutSheet = workbook.getSheet("ratingcut");
+		if (ratingCutSheet != null) {
+			ratingCutVos = new ArrayList<RatingCutVo>();
+			RatingCutVo ratingCutVo;
+			XSSFRow csatExamRatingCutRow;
+			int physicalNumberOfRows = ratingCutSheet.getPhysicalNumberOfRows();
+			for (int rowIndex = 1; rowIndex < physicalNumberOfRows; rowIndex++) {
+				csatExamRatingCutRow = ratingCutSheet.getRow(rowIndex);
+				ratingCutVo = new RatingCutVo();
+				ratingCutVo.setCsatSequence(csatSequence);
+				ratingCutVo.setRatingCode((int) csatExamRatingCutRow.getCell(0).getNumericCellValue());
+				ratingCutVo.setExamSequence(examSequence);
+				ratingCutVo.setSectionSequence((int) csatExamRatingCutRow.getCell(1).getNumericCellValue());  
+				ratingCutVo.setSubjectSequence((int) csatExamRatingCutRow.getCell(2).getNumericCellValue());
+				ratingCutVo.setRawScore((int) csatExamRatingCutRow.getCell(3).getNumericCellValue());
+				ratingCutVo.setStandardScore((int) csatExamRatingCutRow.getCell(4).getNumericCellValue());
+				ratingCutVos.add(ratingCutVo);
+			}
+		}
 	}
 	
 	/**
@@ -33,24 +122,6 @@ public class RatingCutReader {
 	 * @return 영역 리스트
 	 */
 	public List<SectionVo> sectionList() {
-		List<SectionVo> sectionVos = null;
-		XSSFSheet csatExamSectionSheet = workbook.getSheet("csat_exam_section");
-		if (csatExamSectionSheet != null) {
-			sectionVos = new ArrayList<SectionVo>();
-			SectionVo sectionVo;
-			XSSFRow csatExamSectionRow;
-			int physicalNumberOfRows = csatExamSectionSheet.getPhysicalNumberOfRows();
-			for (int rowIndex = 1; rowIndex < physicalNumberOfRows; rowIndex++) {
-				csatExamSectionRow = csatExamSectionSheet.getRow(rowIndex);
-				sectionVo = new SectionVo();
-				sectionVo.setCsatSequence((int) csatExamSectionRow.getCell(0).getNumericCellValue());
-				sectionVo.setExamSequence((int) csatExamSectionRow.getCell(1).getNumericCellValue());
-				sectionVo.setSectionSequence((int) csatExamSectionRow.getCell(2).getNumericCellValue());
-				sectionVo.setSectionName(csatExamSectionRow.getCell(3).getStringCellValue());
-				sectionVo.setSelectCount((int) csatExamSectionRow.getCell(4).getNumericCellValue());
-				sectionVos.add(sectionVo);
-			}
-		}
 		return sectionVos;
 	}
 	
@@ -60,25 +131,6 @@ public class RatingCutReader {
 	 * @return 과목 리스트
 	 */
 	public List<SubjectVo> subjectList() {
-		List<SubjectVo> subjectVos = null;
-		XSSFSheet csatExamSubjectSheet = workbook.getSheet("csat_exam_subject");
-		if (csatExamSubjectSheet != null) {
-			subjectVos = new ArrayList<SubjectVo>();
-			SubjectVo subjectVo;
-			XSSFRow csatExamSubjectRow;
-			int physicalNumberOfRows = csatExamSubjectSheet.getPhysicalNumberOfRows();
-			for (int rowIndex = 1; rowIndex < physicalNumberOfRows; rowIndex++) {
-				csatExamSubjectRow = csatExamSubjectSheet.getRow(rowIndex);
-				subjectVo = new SubjectVo();
-				subjectVo.setCsatSequence((int) csatExamSubjectRow.getCell(0).getNumericCellValue());
-				subjectVo.setExamSequence((int) csatExamSubjectRow.getCell(1).getNumericCellValue());
-				subjectVo.setSectionSequence((int) csatExamSubjectRow.getCell(2).getNumericCellValue());
-				subjectVo.setSubjectSequence((int) csatExamSubjectRow.getCell(3).getNumericCellValue());
-				subjectVo.setSubjectName(csatExamSubjectRow.getCell(4).getStringCellValue());
-				subjectVo.setMaxScore((int) csatExamSubjectRow.getCell(5).getNumericCellValue());
-				subjectVos.add(subjectVo);
-			}
-		}
 		return subjectVos;
 	}
 	
@@ -88,26 +140,6 @@ public class RatingCutReader {
 	 * @return 등급 컷 리스트
 	 */
 	public List<RatingCutVo> ratingCutList() {
-		List<RatingCutVo> ratingCutVos = null;
-		XSSFSheet csatExamRatingCutSheet = workbook.getSheet("csat_exam_rating_cut");
-		if (csatExamRatingCutSheet != null) {
-			ratingCutVos = new ArrayList<RatingCutVo>();
-			RatingCutVo ratingCutVo;
-			XSSFRow csatExamRatingCutRow;
-			int physicalNumberOfRows = csatExamRatingCutSheet.getPhysicalNumberOfRows();
-			for (int rowIndex = 1; rowIndex < physicalNumberOfRows; rowIndex++) {
-				csatExamRatingCutRow = csatExamRatingCutSheet.getRow(rowIndex);
-				ratingCutVo = new RatingCutVo();
-				ratingCutVo.setCsatSequence((int) csatExamRatingCutRow.getCell(0).getNumericCellValue());
-				ratingCutVo.setRatingCode((int) csatExamRatingCutRow.getCell(1).getNumericCellValue());
-				ratingCutVo.setExamSequence((int) csatExamRatingCutRow.getCell(2).getNumericCellValue());
-				ratingCutVo.setSectionSequence((int) csatExamRatingCutRow.getCell(3).getNumericCellValue());  
-				ratingCutVo.setSubjectSequence((int) csatExamRatingCutRow.getCell(4).getNumericCellValue());
-				ratingCutVo.setRawScore((int) csatExamRatingCutRow.getCell(5).getNumericCellValue());
-				ratingCutVo.setStandardScore((int) csatExamRatingCutRow.getCell(6).getNumericCellValue());
-				ratingCutVos.add(ratingCutVo);
-			}
-		}
 		return ratingCutVos;
 	}
 	
@@ -117,25 +149,6 @@ public class RatingCutReader {
 	 * @return 평균 리스트
 	 */
 	public List<AverageVo> averageList() {
-		List<AverageVo> averageVos = null;
-		XSSFSheet csatExamAverageSheet = workbook.getSheet("csat_exam_avg");
-		if (csatExamAverageSheet != null) {
-			averageVos = new ArrayList<AverageVo>();
-			AverageVo averageVo;
-			XSSFRow csatAverageRow;
-			int physicalNumberOfRows = csatExamAverageSheet.getPhysicalNumberOfRows();
-			for (int rowIndex = 1; rowIndex < physicalNumberOfRows; rowIndex++) {
-				csatAverageRow = csatExamAverageSheet.getRow(rowIndex);
-				averageVo = new AverageVo();
-				averageVo.setCsatSequence((int) csatAverageRow.getCell(0).getNumericCellValue());
-				averageVo.setExamSequence((int) csatAverageRow.getCell(1).getNumericCellValue());
-				averageVo.setSectionSequence((int) csatAverageRow.getCell(2).getNumericCellValue());
-				averageVo.setSubjectSequence((int) csatAverageRow.getCell(3).getNumericCellValue());
-				averageVo.setAverage((float) csatAverageRow.getCell(4).getNumericCellValue());
-				averageVo.setStandardDeviation((float) csatAverageRow.getCell(5).getNumericCellValue());
-				averageVos.add(averageVo);
-			}
-		}
 		return averageVos;
 	}
 
