484031085b54b1c416eb0d59995a15010875e992
soonhyeon choi <choissgt@gmail.com>
2016-02-12 22:20:40 +0900

    feat(controller) : update account
    - 비밀번호 인증 기능 구현

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/account/Authentication.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/account/Authentication.java
@@ -1,9 +1,13 @@
 package org.redborn.csatlatte.controller.account;
 
+import org.redborn.csatlatte.commons.servlet.http.HttpSessionValue;
 import org.redborn.csatlatte.commons.tiles.TilesName;
+import org.redborn.csatlatte.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +22,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Authentication {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private HttpSessionValue httpSessionValue;
+	@Autowired
+	private StudentService studentService;
 
 	/**
 	 * 비밀번호를 입력하는 페이지입니다.
 	 */
 	@RequestMapping(method=RequestMethod.GET)
-	public String get() {
+	public String get(Model model) {
 		logger.info("myinfo authentication view");
+		model.addAttribute("studentId", httpSessionValue.getId());
 		return TilesName.PROFILE_AUTHENTICATION_WRITE;
 	}
 	
@@ -36,11 +45,14 @@ public class Authentication {
      * 인증을 실패하였을 경우 비밀번호 입력을 실패 페이지(TilesNane.MYINFO_AUTHENTICATION_FAIL)가 출력 됩니다.
 	 */
 	@RequestMapping(method=RequestMethod.POST)
-	public String post(@RequestParam(value="success",required=false,defaultValue="0") int success) {
+	public String post(Model model, @RequestParam(value="password",required=true) String password) {
 		logger.info("myinfo authentication password wrote");
-		String result = TilesName.PROFILE;
-		if (success != 0) {
-			result = TilesName.PROFILE_AUTHENTICATION_FAIL;
+		String result = TilesName.PROFILE_AUTHENTICATION_FAIL;
+		int studentSequence = httpSessionValue.getStudentSequence();
+		if (studentService.checkPassword(studentSequence, password)) {
+			model.addAttribute("securityQuestionList", studentService.securityQuestionList());
+			model.addAttribute("studentId", httpSessionValue.getId());
+			result = TilesName.PROFILE_SECURITY_WRITE;
 		}
 		return result;
 	}
--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/account/Security.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/account/Security.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,16 +26,6 @@ public class Security {
 	private StudentService studentService;
 	@Autowired
 	private HttpSessionValue httpSessionValue;
-	
-	/**
-	 * 보안질문을 선택하고 보안답변을 입력하는 페이지입니다.
-	 */
-	@RequestMapping(method=RequestMethod.GET)
-	public String get(Model model) {
-		logger.info("myinfo security view");
-		model.addAttribute("securityQuestionList", studentService.securityQuestionList());
-		return TilesName.PROFILE_SECURITY_WRITE;
-	}
 
 	/**
 	 * 보안변경 처리 영역입니다.
