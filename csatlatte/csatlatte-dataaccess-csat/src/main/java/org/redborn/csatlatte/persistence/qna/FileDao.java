56f99c5a031ae35c8c54d8d8737fc39f0bf79480
darkelfand <darkelfand@gmail.com>
2016-02-19 14:27:31 +0900

    feat(dataaccess) : add selectFileName, selectFileCode

--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/qna/FileDao.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/qna/FileDao.java
@@ -6,6 +6,8 @@ import org.redborn.csatlatte.domain.FileVo;
 
 public interface FileDao {
 
+	public String selectFileName(int qnaSequence, int fileSequence);
+	public String selectFileCode(int qnaSequence, int fileSequence);
 	public List<FileVo> selectList(int qnaSequence);
 	public int insert(int qnaSequence, FileVo fileVo);
 	
