9e2c61272e3fb1ecf19ba45db46cef2e6d4a3351
soonhyeon choi <choissgt@gmail.com>
2016-02-26 21:27:24 +0900

    refactor(dataaccess) : update AverageMapper
    - delete 추가

--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/exam/AverageDao.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/exam/AverageDao.java
@@ -8,5 +8,6 @@ public interface AverageDao {
 
 	public List<AverageVo> selectList(int csatSequence, int examSequence);
 	public int insert(AverageVo averageVo);
+	public int delete(int csatSequence, int examSequence);
 	
 }
