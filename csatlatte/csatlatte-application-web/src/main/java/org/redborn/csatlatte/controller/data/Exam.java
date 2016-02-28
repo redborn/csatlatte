855ac65f908e7ef5886ff9c127cc7de84181a0df
soonhyeon choi <choissgt@gmail.com>
2016-02-28 23:09:14 +0900

    refactor(controller) : update data Exam
    - 누락된 model.addAttribute("result") 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/Exam.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/data/Exam.java
@@ -53,11 +53,11 @@ public class Exam {
 		examVo.setYearStudentSequence(yearStudentSequence);
 		examVo.setYmd(ymd);
 		
-		examService.register(examVo);
+		model.addAttribute("result", examService.register(examVo));
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT)
-	public void put(@RequestParam(value="examSequence",required=true) int examSequence,
+	public void put(Model model, @RequestParam(value="examSequence",required=true) int examSequence,
 			@RequestParam(value="csatSequence",required=true) int csatSequence,
 			@RequestParam(value="examName",required=true) String examName,
 			@RequestParam(value="institutionSequence",required=true) int institutionSequence,
@@ -74,13 +74,13 @@ public class Exam {
 		examVo.setYearStudentSequence(yearStudentSequence);
 		examVo.setYmd(ymd);
 		
-		examService.modify(examVo);
+		model.addAttribute("result", examService.modify(examVo));
 	}
 	
 	@RequestMapping(value="{csatSequence}/{examSequence}",method=RequestMethod.DELETE)
-	public void delete(@PathVariable(value="csatSequence") int csatSequence,
+	public void delete(Model model, @PathVariable(value="csatSequence") int csatSequence,
 			@PathVariable(value="examSequence") int examSequence) {
 		logger.info("data manage exam delete");
-		examService.delete(csatSequence, examSequence);
+		model.addAttribute("result", examService.delete(csatSequence, examSequence));
 	}
 }
