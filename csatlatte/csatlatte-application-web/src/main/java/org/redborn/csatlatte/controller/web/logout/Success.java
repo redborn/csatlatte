1bee4de11dff4c886f078b067d72f8b856e28e87
darkelfand <darkelfand@gmail.com>
2016-03-29 18:15:52 +0900

    feat(controller) : update logout
    logout 성공 처리 controller 위치 변경
    logout session invalidate 처리 영역 spring security 로 변경

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/logout/Success.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/logout/Success.java
@@ -1,4 +1,4 @@
-package org.redborn.csatlatte.controller.web;
+package org.redborn.csatlatte.controller.web.logout;
 
 import org.redborn.csatlatte.commons.servlet.http.HttpSessionValue;
 import org.slf4j.Logger;
@@ -7,13 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * 로그아웃입니다.
  */
 @Controller
-@RequestMapping("/logout")
-public class Logout {
+@RequestMapping("/logout/success")
+public class Success {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
@@ -23,10 +24,10 @@ public class Logout {
 	 * 로그아웃입니다.
 	 */
 	@RequestMapping(method=RequestMethod.GET)
-	public String get() {
-		logger.info(new StringBuilder("Controller logout. ID is ").append(httpSessionValue.getId()).toString());;
-		httpSessionValue.invalidate();
-		return "redirect:/main";
+	public String get(RedirectAttributes redirectAttributes) {
+		logger.info("Controller logout success GET.");
+		redirectAttributes.addFlashAttribute("logoutSuccess", true);
+		return "redirect:/login";
 	}
 	
 }
