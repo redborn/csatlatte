f41043e400aabc3c5eea3741015be7edba1fb912
soonhyeon choi <choissgt@gmail.com>
2016-08-03 21:16:21 +0900

    feat(dataaccess) : add QuestionDao
    - 모의고사 문제 Dao 추가

---
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/QuestionDaoMapper.java
@@ -0,0 +1,30 @@
+package org.redborn.csatlatte.persistence;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.redborn.csatlatte.domain.QuestionVo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class QuestionDaoMapper extends SqlSessionDaoSupport implements QuestionDao {
+
+	public List<QuestionVo> selectList(int csatSequence, int examSequence) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("csatSequence", csatSequence);
+		params.put("examSequence", examSequence);
+		
+		return getSqlSession().selectList("question.selectList", params);
+	}
+
+	public int delete(int csatSequence, int examSequence) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("csatSequence", csatSequence);
+		params.put("examSequence", examSequence);
+		
+		return getSqlSession().delete("question.delete", params);
+	}
+
+}
