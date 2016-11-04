5920ea013a15fa1794fc1970a6e9254a3fcca0af
soonhyeon choi <choissgt@gmail.com>
2016-11-04 15:25:56 +0900

    feat(value) : update CorrectAnswerVo
    - 불필요 변수 제거

--- csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/CorrectAnswerVo.java
+++ csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/CorrectAnswerVo.java
@@ -2,45 +2,9 @@ package org.redborn.csatlatte.domain;
 
 public class CorrectAnswerVo {
 
-	private int csatSequence;
-	private int examSequence;
-	private int sectionSequence;
-	private int subjectSequence;
 	private int questionSequence;
 	private int objectItemSequence;
 	private String description;
-	
-	public int getCsatSequence() {
-		return csatSequence;
-	}
-	
-	public void setCsatSequence(int csatSequence) {
-		this.csatSequence = csatSequence;
-	}
-	
-	public int getExamSequence() {
-		return examSequence;
-	}
-	
-	public void setExamSequence(int examSequence) {
-		this.examSequence = examSequence;
-	}
-	
-	public int getSectionSequence() {
-		return sectionSequence;
-	}
-	
-	public void setSectionSequence(int sectionSequence) {
-		this.sectionSequence = sectionSequence;
-	}
-	
-	public int getSubjectSequence() {
-		return subjectSequence;
-	}
-	
-	public void setSubjectSequence(int subjectSequence) {
-		this.subjectSequence = subjectSequence;
-	}
 	
 	public int getQuestionSequence() {
 		return questionSequence;
@@ -65,13 +29,10 @@ public class CorrectAnswerVo {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "CorrectAnswerVo [csatSequence=" + csatSequence
-				+ ", examSequence=" + examSequence + ", sectionSequence="
-				+ sectionSequence + ", subjectSequence=" + subjectSequence
-				+ ", questionSequence=" + questionSequence
+		return "CorrectAnswerVo [questionSequence=" + questionSequence
 				+ ", objectItemSequence=" + objectItemSequence
 				+ ", description=" + description + "]";
 	}
