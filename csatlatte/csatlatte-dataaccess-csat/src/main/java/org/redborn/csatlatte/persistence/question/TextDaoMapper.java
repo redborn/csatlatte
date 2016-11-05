5cbe5b562feaaf9b03929da98e514a402fad6c03
soonhyeon choi <choissgt@gmail.com>
2016-11-05 18:15:16 +0900

    feat(dataaccess) : add TextDao, TextDaoMapper, TextMapper
    - 모의고사 풀기 지문 data access 추가

---
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/question/TextDaoMapper.java
@@ -0,0 +1,23 @@
+package org.redborn.csatlatte.persistence.question;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.redborn.csatlatte.domain.TextVo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TextDaoMapper extends SqlSessionDaoSupport implements TextDao {
+
+	public List<TextVo> selectList(int csatSequence, int examSequence, int sectionSequence, int subjectSequence) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("csatSequence", csatSequence);
+		params.put("examSequence", examSequence);
+		params.put("sectionSequence", sectionSequence);
+		params.put("subjectSequence", subjectSequence);
+		return getSqlSession().selectList("question.text.selectList", params);
+	}
+
+}
