26e0d81a10ac821cf402163b824ad2b42a8f2d68
soonhyeon choi <choissgt@gmail.com>
2016-11-08 16:16:12 +0900

    refactor(dataaccess) : update RatingCutMapper
    - 점수에 해당하는 등급 값을 business에서 계산하지 않고 SQL로 처리하기 위해 수정함
    - RatingCutVoMapForSolving resultMap 삭제
    - selectListDetailForSolving 삭제
    - selectOneRatingByScore 추가

--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/exam/RatingCutDao.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/exam/RatingCutDao.java
@@ -7,9 +7,9 @@ import org.redborn.csatlatte.domain.RatingCutVo;
 
 public interface RatingCutDao {
 	
+	public int selectOneRatingByScore(int score, int csatSequence, int examSequence, int sectionSequence, int subjectSequence);
 	public List<ExamVo> selectList(int csatSequence);
 	public List<RatingCutVo> selectListDetail(int csatSequence, int examSequence);
-	public List<RatingCutVo> selectListDetailForSolving(int csatSequence, int examSequence, int sectionSequence, int subjectSequence);
 	public List<ExamVo> selectListForCreate(int csatSequence);
 	public int delete(int csatSequence, int examSequence);
 	public int insert(RatingCutVo ratingCutVo);
--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/exam/RatingCutDaoMapper.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/exam/RatingCutDaoMapper.java
@@ -13,6 +13,16 @@ import org.springframework.stereotype.Repository;
 public class RatingCutDaoMapper extends SqlSessionDaoSupport implements
 		RatingCutDao {
 
+	public int selectOneRatingByScore(int score, int csatSequence, int examSequence, int sectionSequence, int subjectSequence) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("csatSequence", csatSequence);
+		params.put("examSequence", examSequence);
+		params.put("sectionSequence", sectionSequence);
+		params.put("subjectSequence", subjectSequence);
+		params.put("score", score);
+		return getSqlSession().selectOne("exam.ratingcut.selectOneRatingByScore", params);
+	}
+	
 	public List<ExamVo> selectList(int csatSequence) {
 		return getSqlSession().selectList("exam.ratingcut.selectList", csatSequence);
 	}
@@ -24,16 +34,6 @@ public class RatingCutDaoMapper extends SqlSessionDaoSupport implements
 		
 		return getSqlSession().selectList("exam.ratingcut.selectListDetail", params);
 	}
-	
-	public List<RatingCutVo> selectListDetailForSolving(int csatSequence, int examSequence, int sectionSequence, int subjectSequence) {
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("csatSequence", csatSequence);
-		params.put("examSequence", examSequence);
-		params.put("sectionSequence", sectionSequence);
-		params.put("subjectSequence", subjectSequence);
-		return getSqlSession().selectList("exam.ratingcut.selectListDetailForSolving", params);
-	}
-
 	
 	public List<ExamVo> selectListForCreate(int csatSequence) {
 		return getSqlSession().selectList("exam.ratingcut.selectListForCreate", csatSequence);
