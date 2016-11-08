d3a514cb1ee1c9b28accffeaeae83d090f99d899
soonhyeon choi <choissgt@gmail.com>
2016-11-08 16:05:46 +0900

    update(value) : update TextVo delete contentSequence

--- csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/TextVo.java
+++ csatlatte/csatlatte-value/src/main/java/org/redborn/csatlatte/domain/TextVo.java
@@ -5,7 +5,6 @@ public class TextVo {
 	private int textSequence;
 	private int beginQuestionSequence;
 	private int endQuestionSequence;
-	private int contentSequence;
 	private String content;
 	private String description;
 	
@@ -33,14 +32,6 @@ public class TextVo {
 		this.endQuestionSequence = endQuestionSequence;
 	}
 	
-	public int getContentSequence() {
-		return contentSequence;
-	}
-	
-	public void setContentSequence(int contentSequence) {
-		this.contentSequence = contentSequence;
-	}
-	
 	public String getContent() {
 		return content;
 	}
@@ -56,14 +47,13 @@ public class TextVo {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "TextVo [textSequence=" + textSequence
 				+ ", beginQuestionSequence=" + beginQuestionSequence
-				+ ", endQuestionSequence=" + endQuestionSequence
-				+ ", contentSequence=" + contentSequence + ", content="
+				+ ", endQuestionSequence=" + endQuestionSequence + ", content="
 				+ content + ", description=" + description + "]";
 	}
-	
+
 }
