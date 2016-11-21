2f5f91061085ca231173e3e97aa9e8f318a27d1c
soonhyeon choi <choissgt@gmail.com>
2016-11-21 18:14:04 +0900

    feat(dataaccess) : add ListeningMapper

---
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/exam/subject/ListeningDao.java
@@ -0,0 +1,9 @@
+package org.redborn.csatlatte.persistence.exam.subject;
+
+public interface ListeningDao {
+	
+	public int selectOneCount(int csatSequence, int examSequence, int sectionSequence, int subjectSequence);
+	public String selectOneFileName(int csatSequence, int examSequence, int sectionSequence, int subjectSequence);
+	public String selectOneFileCode(int csatSequence, int examSequence, int sectionSequence, int subjectSequence);
+	
+}
