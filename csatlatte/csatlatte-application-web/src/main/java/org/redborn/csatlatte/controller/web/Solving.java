4336c3912e4955b47e94dfb9d462b09e38303edc
soonhyeon choi <choissgt@gmail.com>
2016-12-06 14:24:36 +0900

    refactor(controller) : update Solving
    - "/solving" POST questionNumber(사용자가 선택한 답) 연산 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/Solving.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/Solving.java
@@ -1,9 +1,13 @@
 package org.redborn.csatlatte.controller.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.redborn.csatlatte.commons.tiles.TilesName;
+import org.redborn.csatlatte.domain.QuestionVo;
 import org.redborn.csatlatte.service.ExamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,14 +54,27 @@ public class Solving {
 	}
 	
 	@RequestMapping(value="{csatSequence}/{examSequence}/{sectionSequence}/{subjectSequence}",method=RequestMethod.POST)
-	public String post(Model model, @PathVariable(value="csatSequence") int csatSequence, @PathVariable(value="examSequence") int examSequence, 
-			@PathVariable(value="sectionSequence") int sectionSequence, @PathVariable(value="subjectSequence") int subjectSequence, 
-			@RequestParam(value="result", required=true) List<Integer> questionNumber) {
+	public String post(Model model, HttpServletRequest request, @PathVariable(value="csatSequence") int csatSequence, @PathVariable(value="examSequence") int examSequence, 
+			@PathVariable(value="sectionSequence") int sectionSequence, @PathVariable(value="subjectSequence") int subjectSequence) {
 		logger.info("Controller solving POST.");
+		List<Integer> questionNumber = new ArrayList<Integer>();
+		List<QuestionVo> questionList = examService.questionList(csatSequence, examSequence, sectionSequence, subjectSequence);
+		if (questionList != null) {
+			int questionListSize = questionList.size();
+			logger.info("questionListSize : " + questionListSize);
+			for (int index = 1; index <= questionListSize; index++) {
+				String parameterValue = request.getParameter(String.valueOf(index));
+				if (parameterValue != null) {
+					questionNumber.add(Integer.parseInt(parameterValue));
+				} else {
+					questionNumber.add(0);
+				}
+			}
+		}
 		int score = examService.calculateScore(questionNumber, csatSequence, examSequence, sectionSequence, subjectSequence);
+		model.addAttribute("questionList", questionList);
 		model.addAttribute("questionNumber", questionNumber);
 		model.addAttribute("correctAnswerList", examService.objectQuestionCorrectAnswerList(csatSequence, examSequence, sectionSequence, subjectSequence));
-		model.addAttribute("questionList", examService.questionList(csatSequence, examSequence, sectionSequence, subjectSequence));
 		model.addAttribute("marking", examService.marking(questionNumber, csatSequence, examSequence, sectionSequence, subjectSequence));
 		model.addAttribute("rating", examService.calculateRating(score, csatSequence, examSequence, sectionSequence, subjectSequence));
 		model.addAttribute("standardScore", examService.calculateStandardScore(score, csatSequence, examSequence, sectionSequence, subjectSequence));
