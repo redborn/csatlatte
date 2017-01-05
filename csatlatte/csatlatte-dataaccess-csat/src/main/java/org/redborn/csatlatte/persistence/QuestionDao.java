0133d0c8536ab4c24f0d762509682885d5169211
soonhyeon choi <choissgt@gmail.com>
2017-01-05 16:42:25 +0900

    feat(dataaccess) : update QuestionMapper
    - selectOne 추가 : '임의 문제 풀기'서비스 등에서 모의고사 문제 1개에 대해서만 조회가 필요한 경우
    - selectListForRadnomsolving 추가 : 사용자가 선택한 조건(학년, 과목)에 맞게 문제 목록을 조회해야하는
    경우

--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/QuestionDao.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/QuestionDao.java
@@ -6,6 +6,8 @@ import org.redborn.csatlatte.domain.QuestionVo;
 
 public interface QuestionDao {
 	
+	public QuestionVo selectOne(int csatSequence, int examSequence, int sectionSequence, int subjectSequence, int questionSequence);
 	public List<QuestionVo> selectList(int csatSequence, int examSequence, int sectionSequence, int subjectSequence);
+	public List<QuestionVo> selectListForRandomsolving(List<Integer> yearStudentSequenceList, List<Integer> subjectSequenceList);
 
 }
--- csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/QuestionDaoMapper.java
+++ csatlatte/csatlatte-dataaccess-csat/src/main/java/org/redborn/csatlatte/persistence/QuestionDaoMapper.java
@@ -11,6 +11,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class QuestionDaoMapper extends SqlSessionDaoSupport implements QuestionDao {
 
+	public QuestionVo selectOne(int csatSequence, int examSequence, 
+			int sectionSequence, int subjectSequence, int questionSequence) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("csatSequence", csatSequence);
+		params.put("examSequence", examSequence);
+		params.put("sectionSequence", sectionSequence);
+		params.put("subjectSequence", subjectSequence);
+		params.put("questionSequence", questionSequence);
+		return getSqlSession().selectOne("question.selectOne", params);
+	}
+	
 	public List<QuestionVo> selectList(int csatSequence, int examSequence,
 			int sectionSequence, int subjectSequence) {
 		Map<String, Object> params = new HashMap<String, Object>();
@@ -19,6 +30,14 @@ public class QuestionDaoMapper extends SqlSessionDaoSupport implements QuestionD
 		params.put("sectionSequence", sectionSequence);
 		params.put("subjectSequence", subjectSequence);
 		return getSqlSession().selectList("question.selectList", params);
+	}
+	
+	public List<QuestionVo> selectListForRandomsolving(List<Integer> yearStudentSequenceList,
+			List<Integer> subjectSequenceList) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("yearStudentSequenceList", yearStudentSequenceList);
+		params.put("subjectSequenceList", subjectSequenceList);
+		return getSqlSession().selectList("question.selectListForRandomsolving", params);
 	}
 
 }
