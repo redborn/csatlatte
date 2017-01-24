98b4aad0de4e1689a9ba7f0012f00a16d4dbf77f
soonhyeon choi <choissgt@gmail.com>
2017-01-24 12:35:36 +0900

    refactor(controller) : update Randomsolving
    - param 값을 그대로 model.addAttribute 처리한 부분 제거
    - POST에서 @PathVariable 파라미터들을 @RequestParam으로 방식 변경

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/Randomsolving.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/Randomsolving.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,35 +24,29 @@ public class Randomsolving {
 	private ExamService examService;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String get(Model model, @RequestParam(value="yearStudentSequenceList", required=true) List<Integer> yearStudentSequenceList,
-			@RequestParam(value="subjectSequenceList", required=true) List<Integer> subjectSequenceList) {
+	public String get(Model model, @RequestParam(value="yearStudentSequence", required=true) List<Integer> yearStudentSequence,
+			@RequestParam(value="subjectSequence", required=true) List<Integer> subjectSequence) {
 		logger.info("Controller randomsolving GET.");
-		QuestionVo randomQuestion = examService.randomQuestion(yearStudentSequenceList, subjectSequenceList);
-		model.addAttribute("yearStudentSequenceList", yearStudentSequenceList);
-		model.addAttribute("subjectSequenceList", subjectSequenceList);
+		QuestionVo randomQuestion = examService.getRandomQuestion(yearStudentSequence, subjectSequence);
 		model.addAttribute("randomQuestion", randomQuestion);
 		model.addAttribute("subjectName", examService.getSubjectName(randomQuestion.getCsatSequence(), randomQuestion.getExamSequence(), randomQuestion.getSectionSequence(), randomQuestion.getSubjectSequence()));
-		TextVo randomQuestionText = examService.text(randomQuestion.getCsatSequence(), randomQuestion.getExamSequence(), randomQuestion.getSectionSequence(), randomQuestion.getSubjectSequence(), randomQuestion.getQuestionSequence());
+		TextVo randomQuestionText = examService.getText(randomQuestion.getCsatSequence(), randomQuestion.getExamSequence(), randomQuestion.getSectionSequence(), randomQuestion.getSubjectSequence(), randomQuestion.getQuestionSequence());
 		if (randomQuestionText != null) {
 			model.addAttribute("randomQuestionText", randomQuestionText);
 		}
 		return TilesName.RANDOMSOLVING_QUESTION;
 	}
 	
-	@RequestMapping(value="{csatSequence}/{examSequence}/{sectionSequence}/{subjectSequence}/{questionSequence}",method=RequestMethod.POST)
-	public String post(Model model, @PathVariable(value="csatSequence") int csatSequence, @PathVariable(value="examSequence") int examSequence,
-			@PathVariable(value="sectionSequence") int sectionSequence, @PathVariable(value="subjectSequence") int subjectSequence, @PathVariable(value="questionSequence") int questionSequence,
-			@RequestParam(value="answer", required=false, defaultValue="0") int answer,
-			@RequestParam(value="yearStudentSequenceList", required=true) List<Integer> yearStudentSequenceList,
-			@RequestParam(value="subjectSequenceList", required=true) List<Integer> subjectSequenceList) {
+	@RequestMapping(method=RequestMethod.POST)
+	public String post(Model model, @RequestParam(value="questionCsatSequence", required=true) int csatSequence, @RequestParam(value="questionExamSequence", required=true) int examSequence,
+			@RequestParam(value="questionSectionSequence", required=true) int sectionSequence, @RequestParam(value="questionSubjectSequence", required=true) int subjectSequence, @RequestParam(value="questionSequence", required=true) int questionSequence,
+			@RequestParam(value="answer", required=false, defaultValue="0") int answer) {
 		logger.info("Controller randomsolving POST.");
 		model.addAttribute("subjectName", examService.getSubjectName(csatSequence, examSequence, sectionSequence, subjectSequence));
-		model.addAttribute("yearStudentSequenceList", yearStudentSequenceList);
-		model.addAttribute("subjectSequenceList", subjectSequenceList);
 		model.addAttribute("randomQuestion", examService.question(csatSequence, examSequence, sectionSequence, subjectSequence, questionSequence));
 		model.addAttribute("marking", examService.marking(answer, csatSequence, examSequence, sectionSequence, subjectSequence, questionSequence));
 		model.addAttribute("correctAnswer", examService.objectQuestionCorrectAnswer(csatSequence, examSequence, sectionSequence, subjectSequence, questionSequence));
-		model.addAttribute("randomQuestionText", examService.text(csatSequence, examSequence, sectionSequence, subjectSequence, questionSequence));
+		model.addAttribute("randomQuestionText", examService.getText(csatSequence, examSequence, sectionSequence, subjectSequence, questionSequence));
 		return TilesName.RANDOMSOLVING_RESULT;
 	}
 
