d4ea4fca5031dab1217c63bbaea4f6aed4d7d166
soonhyeon choi <choissgt@gmail.com>
2017-01-24 12:36:33 +0900

    refactor(controller) : update Select
    - yearStudentList attribute 추가

--- csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/randomsolving/Select.java
+++ csatlatte/csatlatte-application-web/src/main/java/org/redborn/csatlatte/controller/web/randomsolving/Select.java
@@ -1,27 +1,26 @@
 package org.redborn.csatlatte.controller.web.randomsolving;
 
-import java.util.List;
-
 import org.redborn.csatlatte.commons.tiles.TilesName;
+import org.redborn.csatlatte.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/randomsolving/select")
 public class Select {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private StudentService studentService;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String get(Model model, @RequestParam(value="yearStudentSequenceList", required=false, defaultValue="0") List<Integer> yearStudentSequenceList,
-			@RequestParam(value="subjectSequenceList", required=false, defaultValue="0") List<Integer> subjectSequenceList) {
-		model.addAttribute("yearStudentSequenceList", yearStudentSequenceList);
-		model.addAttribute("subjectSequenceList", subjectSequenceList);
+	public String get(Model model) {
+		model.addAttribute("yearStudentList", studentService.yearStudentList());
 		logger.info("Controller randomsolving select GET.");
 		return TilesName.RANDOMSOLVING_SELECT;
 	}
